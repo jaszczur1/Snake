c8124c6a4b2389956cbe12450aa01d97e22f7568
jaszczur1 <m1jadczak@gmail.com>
2016-11-19 02:47:55 +0100

    snake dziala nie dodaje bonus

--- main.java
+++ main.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JFrame;
 
@@ -21,6 +22,9 @@ public class main extends Canvas implements KeyListener {
 
 	int width = 800;
 	int height = 600;
+	
+	int direction; // kierunek pomocnczy snake
+	
 	
 	int[][] board = new int[height/10][width/10];
 
@@ -40,6 +44,8 @@ public class main extends Canvas implements KeyListener {
 		p1.x = 4;
 		p1.y = 2;
 		p1.direction = 39;  // start gry w prawo
+		this.direction = 39;
+		
 		
 		p2.x = 3;
 		p2.y = 2;
@@ -66,9 +72,26 @@ public class main extends Canvas implements KeyListener {
 	}
 
 	void create_board() {
+		
+		int a = -1;
+		int b = -1;
+		boolean bool = false;
+		
 		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board.length; j++) {
+			for (int j = 0; j < board[i].length; j++) {
+				
+				
+				
+				if( board[i][j] ==2) {
+					a = i;
+					b =j;
+					bool = true;
+				}
+				
 				board[i][j] = 0;
+				
+				if (bool)
+				board[a][b]= 2;
 			}
 		}
 	}
@@ -78,7 +101,7 @@ public class main extends Canvas implements KeyListener {
 	void updateSnake(int direction) {
 
 		create_board();
-		
+	
 		if (listPom.isEmpty()) {
 
 			for (int i = 0; i < list.size(); i++) {
@@ -86,7 +109,6 @@ public class main extends Canvas implements KeyListener {
 				Point p = new Point();
 				p.x = list.get(i).x;
 				p.y = list.get(i).y;
-
 				listPom.add(i, p);
 
 			}
@@ -96,8 +118,7 @@ public class main extends Canvas implements KeyListener {
 
 				listPom.get(i).x = list.get(i).x;
 				listPom.get(i).y = list.get(i).y;
-				
-
+						
 			}
 		}
 
@@ -106,6 +127,7 @@ public class main extends Canvas implements KeyListener {
 			Point p = new Point();
 			p.x = list.get(list.size() - 1).x;
 			p.y = list.get(list.size() - 1).y;
+			
 			listPom.add(p);
 		}
 
@@ -122,68 +144,80 @@ public class main extends Canvas implements KeyListener {
 
 		}
 
+		
+	
+		
 		switch (direction) {
-		case 37: // lewo
-
-			if( 39 == list.get(0).direction){
+		
+		case 37: // lewo	
+			
+			if(this.direction == 39){
+				
 				list.get(0).x += 1;
+		
 				break;
 			}
+		
 			
-			list.get(0).x -= 1;
-			list.get(0).direction = 37;
-
-			break;
-
+					
+				list.get(0).x -= 1;
+				list.get(0).direction = 37;
+				this.direction = 37;
+				
+				break;				
+				
 		case 38: // góra
 
-			if( 40 == list.get(0).direction ) {
+			if( this.direction == 40  ) {
 			
 				list.get(0).y += 1;
+				
 			    break;
 			    }
-				
+			
 			list.get(0).y -= 1;
 			list.get(0).direction=38;
+			this.direction=38;
 			
 			break;
-
+		
 		case 39: // prawo
 
-			if( 37 == list.get(0).direction){ 
+			if( this.direction == 37 ){ 
 				
 				list.get(0).x -= 1;
 				
 				break;}
-			
+
+				
 			list.get(0).x += 1;
 			list.get(0).direction=39;
+			this.direction= 39;
 			
 			break;
-
+			
 		case 40: // dół
 			
-			if( 38 == list.get(0).direction){
+			if(this.direction == 38){
+				
 				list.get(0).y -= 1;
+				
 				break;
 			}
 
+
+				
 			list.get(0).y += 1;
 			list.get(0).direction = 40;
+			this.direction = 40;
 			break;
-		}
-
-		
-		
+			}
 		
 		for (int i = 0; i < list.size(); i++) {
 			board[list.get(i).y][list.get(i).x] = 1;
-			
 		}
-		
-		
-		
 	}
+
 
 	void print() {
 
@@ -201,32 +235,101 @@ public class main extends Canvas implements KeyListener {
 		g.setColor(Color.BLACK);
 		
 		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board.length; j++) {
+			for (int j = 0; j < board[i].length; j++) {
 			if(board[i][j] ==1){
 			
 				g.fillRect(j*10,i*10, 10,10);
-				
+				System.out.println(i*10+" "+j*10);
 				}
+			if( board [i][j]==2){
+				g.fillOval(j*10, i*10, 10, 10);
+				
+			}
+			
 			}
 		}
 	}
 	
+	void  setBonus(){
+		
+		
+		boolean czyUstawionoBonus = false;
+		Random r = new Random();
+	     while( !czyUstawionoBonus ){
+		
+		int width =	r.nextInt(this.width/10-20);
+	    int height=	r.nextInt(this.height/10-20);
+		
+	    
+				 if( board[height][width ]!=1 ) {
+					 
+					 board[height][width] = 2; 
+					 czyUstawionoBonus= true;
+					 System.out.println(height+" "+ width);
+					 
+					 }
+	     }		
+	}	
+	
+	void gra() {
+		
+		boolean czyKoniecGry = false;
+		
+		while( !czyKoniecGry ){
+			
+		try {
+			Thread.sleep(1200);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+			
+		
+			if( board[m.list.get(0).y][m.list.get(0).x] == 2 ) {
+				
+				Point p = new Point();
+			    p.x = m.list.get(0).x;
+			    p.y = m.list.get(0).y;
+			    p.direction =  m.list.get(0).direction;
+			    list.add(0, p);
+				System.err.println("drukuj");
+		}
+		
+		  m.updateSnake(m.list.get(0).direction);	
+		  m.repaint();
+		  
+		  
+		  if( m.list.get(0).x*10 == width-10){czyKoniecGry= true;}
+		  if( m.list.get(0).x*10 == 0){czyKoniecGry= true;}
+		 
+		  
+		  if( m.list.get(0).y*10 == height-30){czyKoniecGry= true;}
+		  
+		  if( m.list.get(0).y*10 == 0){czyKoniecGry= true;}
+		   
+		  
+		  
+			
+		}
+		
+	};
+	
 	public static void main(String[] args) {
 
 		m = new main();
+	
 		
+		m.setBonus();
+		
+		m.gra();
 		
 		
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		m.repaint();
-		m.updateSnake(e.getKeyCode());
-		System.out.println("sss");
-		m.print();
-
 		
+		m.list.get(0).direction= e.getKeyCode() ;	
 	}
 
 	@Override
